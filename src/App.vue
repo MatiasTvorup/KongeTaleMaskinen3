9b452d37e6554e991ad67eca6c39628bf0022a38
Matias <Matias@Tvorup.eu>
2025-12-28 15:31:37 +0100

    Taler

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import t2024 from './taler/2024.ts';
+  console.log(t2024);
+</script>
 
 <template>
   <h1>You did it!</h1>
+  <input type="text" placeholder=t2024>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
